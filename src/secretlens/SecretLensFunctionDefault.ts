6e4d48a9f69092ac1a0d0107eed10d16855240f0
fcrespo82 <fernando82@gmail.com>
2016-12-08 13:59:25 -0200

    Remove rot47 implementation and add asking for a password

--- src/secretlens/SecretLensFunctionDefault.ts
+++ src/secretlens/SecretLensFunctionDefault.ts
@@ -7,31 +7,43 @@ export class SecretLensFunctionDefault implements interfaces.ISecretLensFunction
     public password: string;
     public shouldAskForPassword: boolean;
 
-    private rot47(text) {
-        var s = [];
-        for (var i = 0; i < text.length; i++) {
-            var j = text.charCodeAt(i)
-            if ((j >= 33) && (j <= 126)) {
-                s[i] = String.fromCharCode(33 + ((j + 14) % 94))
-            } else {
-                s[i] = String.fromCharCode(j)
-            }
+    constructor() {
+        this.shouldAskForPassword = true;
+    }
+
+    encrypt(inputText: string): Thenable<string> {
+        return this.setPassword().then(() => {
+            const cipher = crypto.createCipher('aes256', this.password);
+            var encrypted = cipher.update(inputText, 'utf8', 'hex');
+            encrypted += cipher.final('hex');
+            return encrypted;
+        });
+    }
+
+    decrypt(inputText: string): Thenable<string> | string {
+        if (this.password == "") {
+            return this.setPassword().then(() => {
+                const decipher = crypto.createDecipher('aes256', this.password);
+                var decrypted = decipher.update(inputText, 'hex', 'utf8');
+                decrypted += decipher.final('utf8');
+                return decrypted;
+            });
+        } else {
+            const decipher = crypto.createDecipher('aes256', this.password);
+            var decrypted = decipher.update(inputText, 'hex', 'utf8');
+            decrypted += decipher.final('utf8');
+            return decrypted;
         }
-        return s.join('')
     }
 
-    encrypt(inputText: string): string {
-        const cipher = crypto.createCipher('aes256', this.password);
-        var encrypted = cipher.update(inputText, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
-        return encrypted;
-    }
-
-    decrypt(inputText: string): string {
-        const decipher = crypto.createDecipher('aes256', this.password);
-        var decrypted = decipher.update(inputText, 'hex', 'utf8');
-        decrypted += decipher.final('utf8');
-
-        return decrypted;
+    setPassword(): Thenable<void> {
+        if (this.shouldAskForPassword && this.password === undefined) {
+            var self = this;
+            return vscode.window.showInputBox({ password: true, prompt: "What's the password to encrypt/decrypt this message?", placeHolder: "password" }).then(function (password) {
+                self.password = password;
+            });
+        } else {
+            return Promise.resolve()
+        }
     }
 }
