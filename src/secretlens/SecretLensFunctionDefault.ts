a29963dd2c6242493f8d0d98a0d2f47e3790bd8b
fcrespo82 <fernando82@gmail.com>
2016-12-08 16:12:53 -0200

    Add salt so crypto api

--- src/secretlens/SecretLensFunctionDefault.ts
+++ src/secretlens/SecretLensFunctionDefault.ts
@@ -4,46 +4,65 @@ import * as vscode from 'vscode'
 
 export class SecretLensFunctionDefault implements interfaces.ISecretLensFunction {
 
-    public password: string;
-    public shouldAskForPassword: boolean;
+    public password: string
+    public shouldAskForPassword: boolean
+    private saltSize: number = 16
 
     constructor() {
-        this.shouldAskForPassword = true;
+        this.shouldAskForPassword = true
     }
 
     encrypt(inputText: string): Thenable<string> {
-        return this.setPassword().then(() => {
-            const cipher = crypto.createCipher('aes256', this.password);
+        return this.setPassword().then((password) => {
+            this.password = password;
+            var salt = crypto.randomBytes(16).toString('hex')
+            const cipher = crypto.createCipher('aes256', salt + this.password);
             var encrypted = cipher.update(inputText, 'utf8', 'hex');
             encrypted += cipher.final('hex');
-            return encrypted;
+            return salt + encrypted;
         });
     }
 
     decrypt(inputText: string): Thenable<string> | string {
-        if (this.password == "") {
-            return this.setPassword().then(() => {
-                const decipher = crypto.createDecipher('aes256', this.password);
+        var salt = inputText.substring(0, 16 * 2)
+        inputText = inputText.replace(salt, "")
+        if (this.password === undefined || this.password === null || this.password === "") {
+            return this.setPassword().then((password) => {
+                this.password = password;
+                const decipher = crypto.createDecipher('aes256', salt + this.password);
                 var decrypted = decipher.update(inputText, 'hex', 'utf8');
                 decrypted += decipher.final('utf8');
                 return decrypted;
             });
         } else {
-            const decipher = crypto.createDecipher('aes256', this.password);
+            const decipher = crypto.createDecipher('aes256', salt + this.password);
             var decrypted = decipher.update(inputText, 'hex', 'utf8');
             decrypted += decipher.final('utf8');
             return decrypted;
         }
     }
 
-    setPassword(): Thenable<void> {
-        if (this.shouldAskForPassword && this.password === undefined) {
+    setPassword(): Thenable<string> {
+        if (this.shouldAskForPassword && (this.password === undefined || this.password === null || this.password === "")) {
             var self = this;
-            return vscode.window.showInputBox({ password: true, prompt: "What's the password to encrypt/decrypt this message?", placeHolder: "password" }).then(function (password) {
-                self.password = password;
+            return vscode.window.showInputBox({
+                password: true, prompt: "What's the password to encrypt/decrypt this message?", placeHolder: "password", ignoreFocusOut: true,
+                validateInput: this.validatePassword
+            }).then(function(password) {
+                return password
             });
         } else {
-            return Promise.resolve()
+            return Promise.resolve(this.password)
         }
+    }
+
+    private validatePassword(password: string): string {
+        if (password === undefined || password === null || password === "") {
+            return "You should provide a password";
+        }
+        if (password.length <= 4) {
+            return "The password should have at least 5 characters";
+        }
+        return undefined
     }
 }
