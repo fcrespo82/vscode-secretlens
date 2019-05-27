ce25c342e6330fa491cab9bafe00d0b1d386efcb
Fernando Crespo <fernando82@gmail.com>
2019-05-27 18:00:14 -0300

    Adopt no reload on install by updating vscode engine

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -14,37 +14,37 @@ import { SecretLensProvider } from '../src/secretlens';
 
 // Defines a Mocha test suite to group tests of similar kind together
 describe("secretlens", () => {
-    var provider: SecretLensProvider
+    var provider: SecretLensProvider;
     before(() => {
         provider = new SecretLensProvider();
 
-        provider.getFunction().setPassword("teste")
+        provider.getFunction().setPassword("teste");
 
     });
 
     context("encrypt", () => {
         it("should encrypt correctly", () => {
-            provider.getFunction().setUseSalt(false)
+            provider.getFunction().setUseSalt(false);
             var tests = [{
                 text: 'test',
                 encrypted: '46800f525d58bb6a3886d37a247bd4db'
-            }]
+            }];
             tests.forEach(test => {
-                assert.equal(provider.getFunction().encrypt(test.text), test.encrypted)
+                assert.equal(provider.getFunction().encrypt(test.text), test.encrypted);
             });
         });
     });
     context("decrypt", () => {
         it("should decrypt correctly", () => {
-            provider.getFunction().setUseSalt(true)
+            provider.getFunction().setUseSalt(true);
             var tests = [
                 {
                     text: 'test',
                     encrypted: '7cdfda208c21275a6188573a4f2476e6c2e89f9579107a813ab8defab1f69956'
                 }
-            ]
+            ];
             tests.forEach(test => {
-                assert.equal(provider.getFunction().decrypt(test.encrypted), test.text)
+                assert.equal(provider.getFunction().decrypt(test.encrypted), test.text);
             });
         });
     });
