cab11214d0f33275ac913db80d242ef81d5aa713
Frederic Ruget <fred@atlant.is>
2020-02-22 00:34:21 +0100

    Add encrypt/decrypt test cases with pbkdf2

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -25,12 +25,19 @@ describe("secretlens", () => {
     context("encrypt", () => {
         it("should encrypt correctly", () => {
             provider.getFunction().setUseSalt(false);
-            var tests = [{
-                text: 'test',
-                encrypted: '46800f525d58bb6a3886d37a247bd4db'
-            }];
+            var tests = [
+                {
+                    method: 'default',
+                    text: 'test',
+                    encrypted: '46800f525d58bb6a3886d37a247bd4db'
+                },{
+                    method: 'pbkdf2',
+                    text: 'test',
+                    encrypted: 'pbkdf2:53616c7465645f5f0000000000000000b455bed4b84f8cbea2afb755feeac6fe'
+                }
+            ];
             tests.forEach(test => {
-                assert.equal(provider.getFunction().encrypt(test.text), test.encrypted);
+                assert.equal(provider.getFunction().encrypt(test.text, test.method), test.encrypted);
             });
         });
     });
@@ -41,6 +48,9 @@ describe("secretlens", () => {
                 {
                     text: 'test',
                     encrypted: '7cdfda208c21275a6188573a4f2476e6c2e89f9579107a813ab8defab1f69956'
+                },{
+                    text: 'test',
+                    encrypted: 'pbkdf2:53616c7465645f5f8e30a5ff83f7d2612164ab18781b486929de0746f3157a27'
                 }
             ];
             tests.forEach(test => {
